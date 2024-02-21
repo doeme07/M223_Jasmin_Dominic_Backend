92f2d26876b455b0f0635cfcc8166c59b91641b3
Dominic Heule <dominic.heule@noseryoung.com>
2024-02-21 13:06:15 +0100

    Adding the exception NoMyListEntryByIdFoundException

---
+++ src/main/java/com/example/demo/core/exception/NoMyListEntryByIdFoundException.java
@@ -0,0 +1,7 @@
+package com.example.demo.core.exception;
+
+public class NoMyListEntryByIdFoundException extends Exception {
+    public NoMyListEntryByIdFoundException(String message) {
+        super(message);
+    }
+}
