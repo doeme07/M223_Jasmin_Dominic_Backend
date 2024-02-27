1332dc41e438d5ab4bf065a6b1ea3149bfa2f0d8
Dominic Heule <dominic.heule@noseryoung.com>
2024-02-27 20:28:56 +0100

    updating the UserPermissionEvaluator, adding the isOwnPost method

--- src/main/java/com/example/demo/core/security/permissionevaluators/UserPermissionEvaluator.java
+++ src/main/java/com/example/demo/core/security/permissionevaluators/UserPermissionEvaluator.java
@@ -1,6 +1,10 @@
 package com.example.demo.core.security.permissionevaluators;
 
+import com.example.demo.domain.mylistentry.MyListEntry;
 import com.example.demo.domain.user.User;
+import com.example.demo.domain.user.UserDetailsImpl;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,4 +17,13 @@ public class UserPermissionEvaluator {
     return true;
   }
 
+  public boolean isOwnPost(MyListEntry targettedMyListEntry) {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    UserDetailsImpl currentUser = (UserDetailsImpl) authentication.getPrincipal();
+    User user = currentUser.user();
+    System.out.println(user.getId());
+    System.out.println(targettedMyListEntry.getUser().getId());
+
+    return user.getId().toString().equals(targettedMyListEntry.getUser().getId().toString());
+  }
 }
